5518edf2c05730fa5c62095c664034a679af95d0
k4r4b3y <128297497+k4r4b3y@users.noreply.github.com>
2023-06-04 16:13:19 +0300

    touch the youtube-dl first

--- src/termux-rice.sh
+++ src/termux-rice.sh
@@ -439,4 +439,5 @@ audio_pref="((bestaudio[acodec=opus]/bestaudio[acodec=vorbis]/bestaudio[acodec=a
 EOF
 
 # symlink yt-dlp to youtube-dl so ytfzf doesn't complain
+touch ${PREFIX}/bin/youtube-dl
 ln -s $(whereis yt-dlp) ${PREFIX}/bin/youtube-dl
